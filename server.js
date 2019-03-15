666df4ccd1cb19b3f172001f796e0655d8bf5eb8
B-Tarik <tarik.bouchekkif@gmail.com>
2019-03-15 17:21:21 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
-
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+
+const errorHandler = require('./handlers/error');
+const apiRoutes = require('./routes/api');
+
+
+const app = express()
+
+
 
 app.use(cors())
 
@@ -18,30 +22,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// routes
+app.use('/api/exercise', apiRoutes);
+
 
 // Not found middleware
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
 })
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
+app.use(errorHandler);
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
