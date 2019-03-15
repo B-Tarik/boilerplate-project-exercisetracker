0c7a7ab44a5eaea7eb015f7250bc2289b974de77
B-Tarik <tarik.bouchekkif@gmail.com>
2019-03-15 17:21:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ const mongoose = require('mongoose')
 const errorHandler = require('./handlers/error');
 const apiRoutes = require('./routes/api');
 
-
 const app = express()
-
-
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -25,14 +21,12 @@ app.get('/', (req, res) => {
 // routes
 app.use('/api/exercise', apiRoutes);
 
-
 // Not found middleware
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
 })
 
 app.use(errorHandler);
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
